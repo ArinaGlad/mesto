38c949d3f641c8cd414145c157c6503900f8f29f
Arina Gladysheva <arina1110@mail.ru>
2022-10-01 00:08:24 +0300

    mediaquery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const edit = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__close');
-
+// Находим форму в DOM
+const formElement = document.querySelector('.popup__container');
+// Находим поля формы в DOM
+const nameInput = document.querySelector('input[name="name"]');
+const jobInput = document.querySelector('input[name="job"]');
+const profileName = document.querySelector('.profile__header');
+const profileJob = document.querySelector('.profile__subheader');
 
 edit.addEventListener('click', function() {
     popup.classList.add('popup_opened');
@@ -10,3 +16,22 @@ edit.addEventListener('click', function() {
 closePopup.addEventListener('click', function() {
     popup.classList.remove('popup_opened');
 });
+
+nameInput.setAttribute('placeholder', `${profileName.textContent}`);
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    popup.classList.remove('popup_opened');
+
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
