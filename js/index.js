667ef552cc76c13341819ec5c8fc6880ae6a1751
Arina Gladysheva <arina1110@mail.ru>
2022-10-09 10:46:33 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -7,6 +7,33 @@ const jobInput = popup.querySelector('input[name="job"]');
 const profileName = document.querySelector('.profile__header');
 const profileJob = document.querySelector('.profile__subheader');
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
 editAuthor.addEventListener('click', function() {
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
