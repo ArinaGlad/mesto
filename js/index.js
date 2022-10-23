5e70c8201418029e5113b6fd35a451fa1f6158bd
Arina Gladysheva <arina1110@mail.ru>
2022-10-24 00:22:58 +0300

    sprint 6

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ const initialCards = [
       const popupProfileOpenButton = document.querySelector('.profile__edit');
       const popupAutor = document.querySelector('.popup-autor');
       const popupMesto = document.querySelector('.popup-mesto');
+      const popupShading = document.querySelectorAll('.popup');
       const switchesPopup = document.querySelectorAll('.popup__close');
       const formElementAutor = document.querySelector('.popup__form-autor');
       const formElementMesto = document.querySelector('.popup__form-mesto');
@@ -42,6 +43,35 @@ const initialCards = [
       const popupImage = document.querySelector('.popup-figure-overlay');
       const popupImageImg = document.querySelector('.popup__img');
       const popupImageText = document.querySelector('.popup__figure-text');
+      const formInput = document.querySelector('.popup__str');
+      const formError = document.querySelector(`.${nameInput.id}-error`);
+
+console.log(`.${nameInput.id}-error`);
+
+
+
+const showInputError = (element, errorMessage) => {
+    element.classList.add('popup__str_type_error');
+    formError.classList.add('popup__str-error_active');
+    formError.textContent = errorMessage;
+};
+      
+
+const hideInputError = (element) => {
+    element.classList.remove('popup__str_type_error');
+    formError.classList.remove('popup__str-error_active');
+    formError.textContent = '';
+};
+      
+const isValid = () => {
+  if (!formInput.validity.valid) {
+    showInputError(formInput, formInput.validationMessage);
+  } else {
+    hideInputError(formInput);
+  }
+};
+
+formInput.addEventListener('input', isValid); 
 
 // создание карточки
 
@@ -93,6 +123,17 @@ switchesPopup.forEach((item) =>
   item.addEventListener('click', (event) => {
     closePopup(event.target.closest('.popup_opened'));
 }));
+
+popupShading.forEach((item) =>
+  item.addEventListener('click', (event) => {
+    closePopup(event.target);
+}));
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closePopup(document.querySelector('.popup_opened'));
+  }
+});
 
 //изменение данных автора
 
